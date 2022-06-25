e165aed2c509a63aa2f8fc198f40866ec551ef71
nail00749 <71979155+nail00749@users.noreply.github.com>
2022-06-25 19:58:22 +0500

    add func to format date

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,14 +1,17 @@
+import {intervalToDuration} from 'date-fns'
+
 const regexp = new RegExp(/^(([^<>()\\[\].,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}])|(([a-zA-Z\-\d]+\.)+[a-zA-Z]{2,}))$/);
 
 export const validateEmail = (email: string): boolean => {
     return regexp.test(email.toLowerCase())
 };
 
-export const noop = () => {}
+export const noop = () => {
+}
 
 export const shuffleArray = <T>(arr: T[]): T[] => {
     let array = [...arr]
-    let currentIndex = array.length,  randomIndex;
+    let currentIndex = array.length, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -17,4 +20,21 @@ export const shuffleArray = <T>(arr: T[]): T[] => {
             array[randomIndex], array[currentIndex]];
     }
     return array
+}
+
+export const formatTime = (time: number): string => {
+    let formattedDate = ''
+    const duration = intervalToDuration({start: 0, end: time * 1000})
+    if (duration.hours) {
+        formattedDate += duration.hours + 'ч.'
+    }
+    if (duration.minutes) {
+        formattedDate += duration.minutes + 'мин.'
+    }
+    if (duration.seconds) {
+        formattedDate += duration.seconds + 'сек.'
+    }
+
+
+    return formattedDate
 }
